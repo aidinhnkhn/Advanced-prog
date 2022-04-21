9b48109176f35141bb673f49e8fb5f853f3c3f02
aidinhnkhn <aidin.khoshneghad@gmail.com>
2022-04-21 11:51:23 +0430

    base of the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module site.edu {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens site.edu to javafx.fxml;
+    exports site.edu;
+    exports Controllers;
+    opens Controllers to javafx.fxml;
+}
---
+++ src/main/java/site/edu/HelloApplication.java
@@ -0,0 +1,25 @@
+package site.edu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root= loader.load();
+        Scene scene = new Scene(root,600,600);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
